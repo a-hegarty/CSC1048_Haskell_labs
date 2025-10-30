3b9992146ab7a95e3ff3f4d8002e5a636476999d
A.Hegarty <a.hegarty004@gmail.com>
2025-10-30 10:25:44 +0000

    added funtins to sumPoly

--- week_3/shortest.hs
+++ week_3/shortest.hs
@@ -1,1 +1,6 @@
-shortest :: [[a]] -> [a] 
+shortest :: [[a]] -> [a]
+shortest [] = []
+shortest [x] = x
+shortest (x: xs) = if length x < length (shortest xs)
+                    then x
+                    else shortest xs
--- week_3/sumPoly.hs
+++ week_3/sumPoly.hs
@@ -1,1 +1,7 @@
+type Poly = [Int]
+
 sumPoly :: Poly -> Poly -> Poly
+sumPoly [] [] = []
+sumPoly x [] = x
+sumPoly [] y = y
+sumPoly (x:xs) (y:ys) = (x + y): sumPoly xs ys
