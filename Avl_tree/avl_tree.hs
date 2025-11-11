6b46c2667d02b58c575be4ceda9b75dffea0168c
A.Hegarty <a.hegarty004@gmail.com>
2025-11-11 14:16:28 +0000

    added initial comments

--- Avl_tree/avl_tree.hs
+++ Avl_tree/avl_tree.hs
@@ -1,5 +1,7 @@
 import Text.PrettyPrint
 
+-- AVL trees take the form Node Root Left right
+-- tree1/2\3\4 will take the form Node 2 (Node 1 empty empty) (Node 3 empty (Node 4 empty empty))
 data AVL_Tree t  = Empty | Node t (AVL_Tree t) (AVL_Tree t)
     deriving(Show, Eq)
 
