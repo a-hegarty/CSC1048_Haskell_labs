af03105e6dad7fcca93ee46141a07c675f69ae61
A.Hegarty <a.hegarty004@gmail.com>
2025-11-12 10:42:31 +0000

    fixed function print_Tree

--- Avl_tree/avl_tree.hs
+++ Avl_tree/avl_tree.hs
@@ -1,13 +1,13 @@
 import Text.PrettyPrint
 
 -- AVL trees take the form Node Root Left right
--- tree1/2\3\4 will take the form Node 2 (Node 1 empty empty) (Node 3 empty (Node 4 empty empty))
+-- tree1/2\3\4 will take the form Node 2 (Node 1 Empty Empty) (Node 3 Empty (Node 4 Empty Empty))
 data AVL_Tree t  = Empty | Node t (AVL_Tree t) (AVL_Tree t)
-    deriving(Show, Eq)
+    deriving(Show, Eq, Ord)
 
-prettyTree :: Show t => AVL_Tree t -> Doc
-prettyTree Empty = text "Empty"
-prettyTree (Node t l r) = text (show t)
-    $+$ nest 1 (prettyTree l)
-    $+$ nest 1 (prettyTree r)
+print_Tree :: Show t => AVL_Tree t -> Doc
+print_Tree Empty = text "Empty"
+print_Tree (Node t l r) = text (show t)
+    $+$ nest 1 (print_Tree l)
+    $+$ nest 1 (print_Tree r)
 
