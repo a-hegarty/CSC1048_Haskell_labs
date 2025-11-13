2176895ab0e7c7712db3d8074a5f860bd5a7232d
A.Hegarty <a.hegarty004@gmail.com>
2025-11-13 12:22:01 +0000

    added height into avl tree data structure

--- Avl_tree/avl_tree.hs
+++ Avl_tree/avl_tree.hs
@@ -2,14 +2,14 @@ import Text.PrettyPrint
 
 -- AVL trees take the form Node Root Left right
 -- tree1/2\3\4 will take the form Node 2 (Node 1 Empty Empty) (Node 3 Empty (Node 4 Empty Empty))
-data AVL_Tree t  = Empty | Node t (AVL_Tree t) (AVL_Tree t)
+data AVL_Tree t  = Empty | Node t (AVL_Tree t) (AVL_Tree t) Int
     deriving(Show, Eq, Ord)
 
 -- when calling, avl tree MUST be in brackets
--- e.g. print_Tree (Node 2 empty empty)
+-- e.g. print_Tree (Node 2 empty empty 0)
 print_Tree :: Show t => AVL_Tree t -> Doc
 print_Tree Empty = text "Empty"
-print_Tree (Node t l r) = text (show t)
+print_Tree (Node t l r _) = text (show t)
     $+$ nest 1 (print_Tree l)
     $+$ nest 1 (print_Tree r)
 
